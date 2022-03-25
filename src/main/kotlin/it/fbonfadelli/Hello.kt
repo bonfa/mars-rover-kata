a7a658681304f73727363f5b369697ffb3c1afbd
ubunfu <bonfa@users.noreply.github.com>
2022-03-25 21:03:18 +0100

    [MARS_ROVER] initial commit

---
+++ src/main/kotlin/it/fbonfadelli/Hello.kt
@@ -0,0 +1,5 @@
+package it.fbonfadelli
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
---
+++ src/test/kotlin/it/fbonfadelli/HelloTest.kt
@@ -0,0 +1,11 @@
+package it.fbonfadelli
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class HelloTest {
+    @Test
+    internal fun `it works`() {
+        assertThat(true).isEqualTo(true)
+    }
+}
