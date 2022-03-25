a14675ebb4756540e150e31645b40cefc5e87437
ubunfu <bonfa@users.noreply.github.com>
2022-03-25 21:04:59 +0100

    [MARS_ROVER] define axis of complexities and start writing the first tests

--- src/test/kotlin/it/fbonfadelli/HelloTest.kt
+++ src/test/kotlin/it/fbonfadelli/HelloTest.kt
@@ -4,8 +4,47 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class HelloTest {
-    @Test
-    internal fun `it works`() {
-        assertThat(true).isEqualTo(true)
+
+/*
+AXIS
+  - command [F,B,L,R]
+  - number of commands [1,2, ..., n]
+  - world dimension [1,2]
+  -
+
+* hardcoded starting position and direction - move forward only - x-axis only
+* different starting position - move forward only - x-axis only
+* */
+
+  @Test
+  internal fun `hardcoded starting position and direction - move forward only - x-axis only`() {
+      val rover = Rover()
+      rover.moveForward()
+
+      val position = rover.getX()
+
+      assertThat(position).isEqualTo(1)
+  }
+
+  @Test
+  internal fun `different starting position - move forward only - x-axis only`() {
+    val rover = Rover(1)
+    rover.moveForward()
+
+    val position = rover.getX()
+
+    assertThat(position).isEqualTo(2)
+  }
+
+  class Rover(initialX: Int = 0) {
+    private var x: Int = initialX
+
+      fun moveForward() {
+          this.x = this.x + 1
+        }
+
+        fun getX():Int  {
+            return x
+        }
     }
 }
